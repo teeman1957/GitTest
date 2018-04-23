c1a8a5e317f918354f644f9836068761bd17c5a5
Anthony Shaw <teeman1957@charter.net>
2018-04-23 08:54:48 -0500

    Initial Commit

---
+++ GitTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitTest
+//
+//  Created by Anthony Shaw on 4/23/18.
+//  Copyright © 2018 Anthony Shaw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Anthony Shaw on 4/23/18.
+//  Copyright © 2018 Anthony Shaw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
